4c0b23d3938bb0b8731329975206bb63a7046932
Agustin800 <aguslahalo@gmail.com>
2025-08-01 18:58:39 +0000

    task 4, todo joya

--- part4/base_files/scripts.js
+++ part4/base_files/scripts.js
@@ -1,20 +1,40 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
   if (window.location.href.includes("index.html")) {
+    checkAuthentication();
     fetch_places("All");
     loadPriceFilter();
   }
   if (window.location.href.includes("place.html")) {
-    const placeToken = JSON.parse(localStorage.getItem("place"));
-    storeUserById(placeToken.owner_id);
-    const ownerToken = JSON.parse(localStorage.getItem("owner"));
-    displayPlaceDetails(placeToken, ownerToken);
-  }
+    const params = new URLSearchParams(window.location.search);
+    const placeId = params.get("place_id");
 
+    if (placeId) {
+      fetchPlaceAndOwner(placeId);
+    } else {
+      alert("No place_id found in URL");
+    }
+  }
+  if (window.location.href.includes("add_review.html")) {
+    const token = getCookie('token');
+    if (!token) {
+      window.location.href = 'index.html';
+    }
+
+    loadSelectOptions();
+
+    const reviewForm = document.getElementById('review-form');
+    const place_id = getPlaceIdFromURL();
+
+    if (reviewForm) {
+      reviewForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        let rating = Number(document.querySelector("#slcRating").value);
+        let text = document.querySelector("#review").value;
+
+         addReview(text, rating, place_id, token);
+      });
+    }
+  }
 
   const loginForm = document.getElementById('login-form');
 
@@ -70,6 +90,8 @@ function checkAuthentication() {
     // Fetch places data if the user is authenticated
     fetch_places("All");
   }
+
+
 }
 function getCookie(name) {
   const value = `; ${document.cookie}`;
@@ -141,9 +163,7 @@ async function fetchPlaceDetails(place_id) {
     })
 
     if (response.ok) {
-      const place = await response.json();
-      localStorage.setItem("place", JSON.stringify(place));
-      window.location.href = "place.html";
+      window.location.href = `place.html?place_id=${place_id}`;
     } else {
       alert(`Failed to retrieve the place with the ID: ${place_id}: ` + response.statusText);
     }
@@ -175,7 +195,6 @@ function displayReviewLink() {
 }
 
 async function displayPlaceDetails(place, owner) {
-
   let placeContainer = document.querySelector("#place-details");
   placeContainer.innerHTML = "";
   placeContainer.innerHTML += `<div class="place-info">
@@ -200,16 +219,18 @@ async function displayPlaceDetails(place, owner) {
 
   for (let i = 0; i < place.reviews.length; i++) {
     let review = place.reviews[i];
+    let rating = review.rating
     const reviwerName = await getUserById(review.user);
     reviewsContainer.innerHTML += `
     <div class="review-card">
     <p><strong>${reviwerName}</strong></p>
-    <p><strong>Rating: </strong>${review.rating}</p>
+    <p><strong>Rating: </strong>${'✡'.repeat(rating)}</p>
     <p><strong>Comment: </strong>${review.text}</p>
     </div>
     `;
   }
-  placeContainer.innerHTML += `<a href="add_review.html" id="add-review-link">Add Reviews</a>`;
+
+  placeContainer.innerHTML += `<a href="add_review.html?place_id=${getPlaceIdFromURL()}" id="add-review-link">Add Reviews</a>`;
   displayReviewLink();
 }
 
@@ -221,9 +242,69 @@ async function getUserById(user_id) {
   })
 
   if (response.ok) {
-    const user = await response.json();  
-    return `${user.first_name} ${user.last_name}`;  ;
+    const user = await response.json();
+    return `${user.first_name} ${user.last_name}`;;
   } else {
     alert(`Failed to retrieve the user with the ID: ${user_id}: ` + response.statusText);
+  }
+}
+
+function getPlaceIdFromURL() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("place_id");
+}
+
+async function fetchPlaceAndOwner(placeId) {
+  const response = await fetch(`http://127.0.0.1:5000/api/v1/places/${placeId}`, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  if (response.ok) {
+    const place = await response.json();
+    const ownerResponse = await fetch(`http://127.0.0.1:5000/api/v1/users/${place.owner_id}`, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    if (ownerResponse.ok) {
+      const owner = await ownerResponse.json();
+      displayPlaceDetails(place, owner);
+    } else {
+      alert("Failed to load owner info");
+    }
+  } else {
+    alert("Failed to load place details");
+  }
+}
+
+function loadSelectOptions() {
+  let selectContainer = document.querySelector("#slcRating");
+  selectContainer.innerHTML = "";
+  selectContainer.innerHTML += `<option value="1">✡</option>
+  <option value="2">✡✡</option>
+  <option value="3">✡✡✡</option>
+  <option value="4">✡✡✡✡</option>
+  <option value="5">✡✡✡✡✡</option>`
+}
+
+ async function addReview(text, rating, place_id, token) {
+  let response = await fetch("http://127.0.0.1:5000/api/v1/reviews/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify({ text, rating, place_id })
+  })
+
+  if (response.ok) {
+    alert('Review added successfully');
+    document.querySelector("#review").value = "";
+  } else {
+    alert('Could not add review : ' + response.statusText);
+
   }
 }
